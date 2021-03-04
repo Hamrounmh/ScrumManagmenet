c0860df8de909c7cd0fa0f627817b1bbf2ab4b6a
hamrounmh <hamrounhoussem@gmail.com>
2021-03-04 15:39:05 +0100

    seconde commit

--- TP1/src/test/java/testAnimal.java
+++ TP1/src/test/java/testAnimal.java
@@ -5,30 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * Classe-test testAnimal.
- *
- * @author  (votre nom)
- * @version (un numéro de version ou une date)
- *
- * Les classes-test sont documentées ici :
- * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
- * et sont basées sur le document Š 2002 Robert A. Ballance intitulé
- * "JUnit: Unit Testing Framework".
- *
- * Les objets Test (et TestSuite) sont associés aux classes à tester
- * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
- * se nommera NameTest.java); les deux se retrouvent dans le męme paquetage.
- * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
- * qui sont vraies pour chaque méthode Test à exécuter.  Il peut y avoir
- * plus d'une méthode Test dans une classe Test; leur ensemble forme un
- * objet TestSuite.
- * BlueJ découvrira automatiquement (par introspection) les méthodes
- * Test de votre classe Test et générera la TestSuite conséquente.
- * Chaque appel d'une méthode Test sera précédé d'un appel de setUp(),
- * qui réalise les engagements, et suivi d'un appel à tearDown(), qui les
- * détruit.
- */
+
 public class testAnimal
 {
     // Définissez ici les variables d'instance nécessaires à vos engagements;
