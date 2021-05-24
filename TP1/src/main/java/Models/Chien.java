4fa3e3417999d108ea25f3ac74300763f79de36f
Ahmed <ahmed.horri@dauphine.eu>
2021-05-24 23:18:08 +0200

    update class chien

--- TP1/src/main/java/Models/Chien.java
+++ TP1/src/main/java/Models/Chien.java
@@ -12,14 +12,14 @@ public class Chien extends Animal{
     public Chien(int vitesse, int poids) {
         super(vitesse,poids);
     }
-    public Chien(int vitesse, int poids, ArrayList<Nouriture> nouriture){
-        super(vitesse,poids,nouriture);
+    public Chien(int vitesse, int poids, ArrayList<Nourriture> nourriture){
+        super(vitesse,poids, nourriture);
     }
 
     @Override
     public boolean manger(int quantite) throws NourritureNotFundExcepton {
-        if(super.nouriture!=null){
-            super.poids=super.nouriture.stream().map(x -> x.getCoeff()* super.poids * quantite).reduce(0,(x,y) -> x+y);
+        if(super.nourriture !=null){
+            super.poids=super.nourriture.stream().map(x -> x.getCoeff()* super.poids * quantite).reduce(0,(x, y) -> x+y);
             super.vitesse+= super.vitesse * 0.3;
             return true;
         }else{
